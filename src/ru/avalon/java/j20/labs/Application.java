2ebfdd45346ce3f07e76c3888ef29b6ef61d07f0
Яргин Юрий <yargin.y@gmail.com>
2019-11-01 12:05:46 +0300

    done visual interface

--- src/ru/avalon/java/j20/labs/Application.java
+++ src/ru/avalon/java/j20/labs/Application.java
@@ -1,10 +1,11 @@
 package ru.avalon.java.j20.labs;
 
-import ru.avalon.java.j20.labs.frames.AbstractFrame;
 import ru.avalon.java.j20.labs.frames.Calculator;
+
+import javax.swing.*;
 
 public class Application {
     public static void main(String[] args) {
-        AbstractFrame mainFrame = new Calculator();
+        JFrame mainFrame = new Calculator();
     }
 }
--- src/ru/avalon/java/j20/labs/frames/Calculator.java
+++ src/ru/avalon/java/j20/labs/frames/Calculator.java
@@ -3,46 +3,81 @@ package ru.avalon.java.j20.labs.frames;
 import javax.swing.*;
 import java.awt.*;
 
-public class Calculator extends AbstractFrame {
+public class Calculator extends JFrame {
+    private static final int GAP = 10;
+    private static final GridLayout GRID = new GridLayout(1, 1);
+    static {
+        GRID.setVgap(GAP);
+        GRID.setHgap(GAP);
+    }
+
     JLabel result = new JLabel("0");
-    JPanel buttons = new JPanel();
-    int elemHeight;
 
     public Calculator() throws HeadlessException {
         super("Calculator");
+        setSize(250, 400);
+        setLocationByPlatform(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         initElems();
+        setVisible(true);
     }
 
     private void initElems() {
-        setLayout(new BorderLayout());
+        //init main panel, configure side padding and padding between rows
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(GAP, GAP, GAP, GAP));
+        GridLayout gridAll = new GridLayout(6, 1);
+        gridAll.setVgap(GAP);
+        mainPanel.setLayout(gridAll);
 
-        JPanel top = new JPanel();
-        top.setMinimumSize(new Dimension(20, 100));
-        top.add(result);
-        add(top, BorderLayout.PAGE_START);
+        //add button rows
+        mainPanel.add(makeResultRow());
+        mainPanel.add(makeRow("7", "8", "9", "+"));
+        mainPanel.add(makeRow("4", "5", "6", "-"));
+        mainPanel.add(makeRow("1", "2", "3", "*"));
+        mainPanel.add(makeRow("CE", "0", ".", "/"));
+        mainPanel.add(makeRow("="));
 
-        buttons.setLayout(new GridLayout(4, 4));
-        initButtons();
-        add(buttons, BorderLayout.CENTER);
-
-        add(createButton("="), BorderLayout.PAGE_END);
+        add(mainPanel);
     }
 
-    private void initButtons() {
-        buttons.add(createButton("0"));
-        buttons.add(createButton("1"));
-        buttons.add(createButton("2"));
-        buttons.add(createButton("3"));
-        buttons.add(createButton("4"));
-        buttons.add(createButton("5"));
-        buttons.add(createButton("6"));
-        buttons.add(createButton("7"));
-        buttons.add(createButton("9"));
-        buttons.add(createButton("10"));
+    /**
+     * create four buttons row
+     * @return panel with four buttons
+     */
+    private JPanel makeRow(String button1, String button2, String button3, String button4){
+        JPanel row = new JPanel();
+        row.setLayout(GRID);
+        row.add(createButton(button1));
+        row.add(createButton(button2));
+        row.add(createButton(button3));
+        row.add(createButton(button4));
+        return row;
     }
 
-    private int getButHeight() {
-        return createButton("size").getHeight();
+    /**
+     * create one button row
+     * @return panel with one button
+     */
+    private JPanel makeRow(String button){
+        JPanel row = new JPanel();
+        row.setLayout(GRID);
+        row.add(createButton(button));
+        return row;
+    }
+
+    /**
+     * create and configure result panel
+     * @return panel with result
+     */
+    private JPanel makeResultRow(){
+        Font font = new Font("TimesRoman", Font.PLAIN, 30);
+        result.setHorizontalAlignment(SwingConstants.RIGHT);
+        result.setFont(font);
+        JPanel row = new JPanel();
+        row.setLayout(GRID);
+        row.add(result);
+        return row;
     }
 
     private JButton createButton(String label) {
